27969a1c945f5f949a5722a0220239196a11e538
Ronald Amoo <butsormeamoo@gmail.com>
2021-03-24 14:20:22 +0000

    added new API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { reviews: [] };
+  }
+  componentDidMount() {
+    fetch(
+      "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=1WYyTafv0tdwGfQVQ1DKB4CmDr8doR0l"
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res.results);
+        this.setState({ reviews: res.results });
+      })
+      .catch((err) => console.log(err));
+  }
+  render() {
+    return (
+      <div>
+        {this.state.reviews.map((result) => {
+          return (
+            <div>
+              <h1>New York Movie Review</h1>
+              <hr />
+              <b>Title</b>
+              <h1>{result.display_title}</h1>
+              <b>Release date</b>
+              <h2>
+                {result.publication_date}
+                {""}
+              </h2>
+              <b>Critics</b>
+              <h3>{result.byline}</h3>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
-
-export default App;
